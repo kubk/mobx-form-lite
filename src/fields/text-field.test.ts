794f43a78221d9a17cfe79844b8abd3091d4550a
Gorbachev Egor <7gorbachevm@gmail.com>
2024-05-04 16:18:52 +0700

    Proove that TextField validation is lazy

--- src/fields/text-field.test.ts
+++ src/fields/text-field.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { TextField } from "./text-field";
+import { autorun } from "mobx";
 
 describe("TextField", () => {
   test("value / onChange", () => {
@@ -47,5 +48,32 @@ describe("TextField", () => {
     field.onChange("new value");
     expect(cloned.value).toBe("");
     expect(cloned.error).toBe("error");
+  });
+
+  test("validation is lazy", () => {
+    let calledTimes = 0;
+
+    const field = new TextField("", {
+      validate: (value) => {
+        calledTimes++;
+        return value ? undefined : "Required";
+      },
+    });
+    field.onChange("val 1");
+    field.onChange("val 2");
+    expect(calledTimes).toBe(0);
+
+    autorun(() => {
+      if (field.isTouched) {
+        field.error;
+      }
+    });
+
+    field.onChange("val 3");
+    expect(calledTimes).toBe(0);
+
+    field.onBlur();
+    field.onChange("val 4");
+    expect(calledTimes).toBe(2);
   });
 });
